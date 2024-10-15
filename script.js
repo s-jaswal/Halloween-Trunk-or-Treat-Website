ead75dfe9c94d658e0919366bd73bfb1b893da4c
s-jaswal <sjasdocs@gmail.com>
2024-10-15 19:14:27 -0400

    finished the basic layout for all pages - now just to add styling and make it look good - taekwondo break yayyy

---
+++ script.js
@@ -0,0 +1,20 @@
+// for the trunk showcase page
+$(document).ready(function () {
+    $('.trunk-image').on('mouseenter', function () {
+        // Find the sibling description of the hovered image and fade it in
+        $(this).siblings('.trunk-description').fadeIn(300);
+    });
+
+    $('.trunk-image').on('mouseleave', function () {
+        // Find the sibling description of the hovered image and fade it out
+        $(this).siblings('.trunk-description').fadeOut(300);
+    });
+});
+
+// for the schedule (3rd page)
+$(document).ready(function () {
+    // When an event title is clicked, toggle the corresponding details
+    $('.event-title').on('click', function () {
+        $(this).next('.event-details').slideToggle(300);
+    });
+});
